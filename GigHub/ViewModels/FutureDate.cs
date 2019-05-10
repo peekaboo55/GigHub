46d64c5c4200431894374f4981265c0c349eece6
Peerawat <peejenpq@gmail.com>
2019-05-10 19:45:04 +0700

    update

--- GigHub/ViewModels/FutureDate.cs
+++ GigHub/ViewModels/FutureDate.cs
@@ -10,7 +10,7 @@ namespace GigHub.ViewModels
         {
             DateTime dateTime;
             var isValid = DateTime.TryParseExact(Convert.ToString(value),
-                "d MMM yyyy",
+                "dd MM yyyy",
                 CultureInfo.CurrentCulture,
                 DateTimeStyles.None,
                 out dateTime);
